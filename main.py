522a1eef4bb14c0bee07eeeb383d402bc7f039eb
Karen dos Anjos Arcoverde <karenarcoverde@poli.ufrj.br>
2024-01-06 16:26:15 -0300

    added griddata

--- main.py
+++ main.py
@@ -15,21 +15,26 @@ response = requests.post(url, json=data)
 if response.status_code == 200:
     data = response.json()
     print("Dados recebidos com sucesso!")
+
+    latitud = [feature['geometry']['coordinates'][1] for feature in data['features']]
+    longitud = [feature['geometry']['coordinates'][0] for feature in data['features']]
+    intensity = [feature['properties']['intensity'] for feature in data['features']]
+
+    minIntensity = min(intensity)
+    maxIntensity = max(intensity)
+
+    normalized_intensity = [(i - minIntensity) / (maxIntensity - minIntensity) for i in intensity]
+
+    grid_lat, grid_lon = np.mgrid[min(latitud):max(latitud):100j, min(longitud):max(longitud):100j]
+
+    grid_intensity = griddata((latitud, longitud), normalized_intensity, (grid_lat, grid_lon), method='cubic')
+
+    heatmap_data = [[grid_lat[i][j], grid_lon[i][j], grid_intensity[i][j]] for i in range(100) for j in range(100) if not np.isnan(grid_intensity[i][j])]
+
+    map = folium.Map(location=[-22.9068, -43.1729], zoom_start=10)
+
+    HeatMap(heatmap_data, min_opacity=0.5, max_zoom=18, radius=20).add_to(map)
+
+    map.save('heatmap.html')
 else:
     print("Erro na requisição:", response.status_code)
-
-latitud = [feature['geometry']['coordinates'][1] for feature in data['features']]
-longitud = [feature['geometry']['coordinates'][0] for feature in data['features']]
-intensity = [feature['properties']['intensity'] for feature in data['features']]
-
-grid_lat, grid_lon = np.mgrid[min(latitud):max(latitud):100j, min(longitud):max(longitud):100j]
-
-grid_intensity = griddata((latitud, longitud), intensity, (grid_lat, grid_lon), method='cubic')
-
-heatmap_data = [[grid_lat[i][j], grid_lon[i][j], grid_intensity[i][j]] for i in range(100) for j in range(100) if not np.isnan(grid_intensity[i][j])]
-
-map = folium.Map(location=[-22.9068, -43.1729], zoom_start=10)
-
-HeatMap(heatmap_data).add_to(map)
-
-map.save('heatmap.html')
