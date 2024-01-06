1f64c8fae811f2ea0f4f502baa001e2737ac961a
Karen dos Anjos Arcoverde <karenarcoverde@poli.ufrj.br>
2024-01-06 17:22:59 -0300

    interpolated interval

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ data = {
 }
 
 response = requests.post(url, json=data)
-
 if response.status_code == 200:
     data = response.json()
     print("Dados recebidos com sucesso!")
@@ -29,10 +28,13 @@ if response.status_code == 200:
 
     grid_intensity = griddata((latitud, longitud), normalized_intensity, (grid_lat, grid_lon), method='cubic')
 
-    interpolated_heatmap_data = [[grid_lat[i][j], grid_lon[i][j], grid_intensity[i][j]] for i in range(100) for j in range(100) if not np.isnan(grid_intensity[i][j])]
+    flat_grid_intensity = grid_intensity.flatten()
+    normalized_grid_intensity = [(i - np.nanmin(flat_grid_intensity)) / (np.nanmax(flat_grid_intensity) - np.nanmin(flat_grid_intensity)) for i in flat_grid_intensity]
+
+    interpolated_heatmap_data = [[grid_lat.flatten()[i], grid_lon.flatten()[i], normalized_grid_intensity[i]] for i in range(len(normalized_grid_intensity)) if not np.isnan(normalized_grid_intensity[i])]
 
     original_heatmap_data = [[lat, lon, inten] for lat, lon, inten in zip(latitud, longitud, intensity)]
-
+    print(interpolated_heatmap_data)
     combined_heatmap_data = original_heatmap_data + interpolated_heatmap_data
 
     map = folium.Map(location=[-22.9068, -43.1729], zoom_start=10)
