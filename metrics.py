a0e2fe5c0018dc623760627d00f45e3971df2391
Karen dos Anjos Arcoverde <karenarcoverde@poli.ufrj.br>
2024-02-14 21:22:58 -0300

    added silhouette score

--- metrics.py
+++ metrics.py
@@ -6,3 +6,60 @@ print(accuracy)
 
 correlation = data['predict'].corr(data['correct'])
 print(correlation)
+
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+from sklearn.cluster import KMeans
+import os, shutil, glob, os.path
+from PIL import Image as pil_image
+image.LOAD_TRUNCATED_IMAGES = True 
+model = VGG16(weights='imagenet', include_top=False)
+
+# Variables
+imdir = './dataset/'
+targetdir = "./outdir/"
+number_clusters = 3
+
+# Loop over files and get features
+filelist = glob.glob(os.path.join(imdir, '*.jpeg'))
+filelist.sort()
+featurelist = []
+for i, imagepath in enumerate(filelist):
+    print("    Status: %s / %s" %(i, len(filelist)), end="\r")
+    img = image.load_img(imagepath, target_size=(224, 224))
+    img_data = image.img_to_array(img)
+    img_data = np.expand_dims(img_data, axis=0)
+    img_data = preprocess_input(img_data)
+    features = np.array(model.predict(img_data))
+    featurelist.append(features.flatten())
+
+# Clustering
+kmeans = KMeans(n_clusters=number_clusters, random_state=0).fit(np.array(featurelist))
+
+# Copy images renamed by cluster 
+# Check if target dir exists
+try:
+    os.makedirs(targetdir)
+except OSError:
+    pass
+# Copy with cluster name
+print("\n")
+for i, m in enumerate(kmeans.labels_):
+    print("    Copy: %s / %s" %(i, len(kmeans.labels_)), end="\r")
+    shutil.copy(filelist[i], targetdir + str(m) + "_" + str(i) + ".jpeg")
+
+from sklearn.metrics import silhouette_score
+
+featurelist_array = np.array(featurelist)
+
+silhouette_scores = []
+for n_clusters in range(2, 11):  # De 2 a 10 clusters
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(featurelist_array)
+    score = silhouette_score(featurelist_array, kmeans.labels_)
+    silhouette_scores.append(score)
+    print(f"Índice Médio de Silhueta para {n_clusters} clusters: {score}")
+
+# O resultado será uma lista de valores do Índice de Silhueta para cada número de clusters
+print(silhouette_scores)
